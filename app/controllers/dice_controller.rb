f15a0d4f610d641ca1b25a4ef76259494edf35d8
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-02-13 02:12:57 +0000

    craps

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,22 @@
-class DiceController < ApplicationController
+<div> class="header">
+  <h1>Jonathon's Online Craps Table</h1>
+</div>
 
-end
+<% if session[:point].nil? %>
+  <p>This is your first roll.</p>
+<% else %>
+  <p>The point is <%= session[:point] %>. You need to roll this again to win.</p>
+<% end %>
+
+<img src="/images/dice/<%= @die1 %>.svg" alt="">
+<img src="/images/dice/<%= @die2 %>.svg" alt="">
+
+<p>You rolled a <%= @die1 %> and a <%= @die2 %>. The total is <%= @total %>.</p>
+
+<p><%= @result %></p>
+
+<% if session[:game_over] %>
+  <button onclick="window.location.href='/roll_dice'">Start New Game</button>
+<% else %>
+  <button onclick="window.location.href='/roll_again'">Roll Again</button>
+<% end %>
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   get("/tacos", :controller => "tacos", :action => "index")
   get("/dice", :controller => "dice", :action => "roll")
   get("/roll_dice", :controller => "roll_dice", :action => "index")
+  get("/welcome", :controller => "welcome", :action => "index")
+  get("/place_bet", :controller => "place_bet", :action => "index")
+  get("/pass_line", :controller => "pass_line", action => "index")
+  get("/non_pass_line", :controller => "non_pass_line", action => "index")
 end
