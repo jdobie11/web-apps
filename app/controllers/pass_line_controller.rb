af105d04b8dcad07a4e233b3ccabc37faacdee43
jdobie11 dobes <jonathon.dobie@kellogg.northwestern.edu>
2025-02-13 02:33:50 +0000

    trying

--- app/controllers/pass_line_controller.rb
+++ app/controllers/pass_line_controller.rb
@@ -1,2 +1,12 @@
 class PassLineController < ApplicationController
+
+    def pass_line
+      session[:game_over] = false
+      session[:point] = nil
+      redirect_to roll_dice_path, notice: 'You are playing the pass line.'
+    end
+  
+    # Existing actions
+
+  
 end
